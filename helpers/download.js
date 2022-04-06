7aa447a37ce6e6cad52516eb5e857296370e8a38
Joachim Hofman <joachim@cbb.nl>
2022-04-06 09:49:24 +0200

    Updated with Pinky Test and Bugfixes

---
+++ helpers/download.js
@@ -0,0 +1,12 @@
+
+function download(url, dest, cb) {
+    fetch(url).then((response) => {
+        var destination = fs.createWriteStream(dest);
+        response.body.pipe(destination);
+        destination.on("finish", function () {
+            cb();
+        });
+    });
+}
+
+module.exports = download;
